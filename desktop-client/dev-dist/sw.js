2068116bba8e2adbaf050d883f034f6b1476f1a5
bibekchandsah <bibeksha48@gmail.com>
2026-03-10 11:54:03 +0530

    device management fixed

--- desktop-client/dev-dist/sw.js
+++ desktop-client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-378048b6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r4rfpsr40ao"
+    "revision": "0.30j5r2se0vs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
